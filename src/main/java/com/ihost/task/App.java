96b973e3bb1a945b05a779405d800f2498df3193
Armen Arakelyan <arakel.armen.88@gmail.com>
2020-04-08 12:03:15 +0400

    Chenged to close resources

--- src/main/java/com/ihost/task/App.java
+++ src/main/java/com/ihost/task/App.java
@@ -6,12 +6,15 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+        int producersCount;
+        int consumersCount;
+        try(Scanner scan = new Scanner(System.in)) {
 
-        System.out.print("Producers count: ");
-        int producersCount = scan.nextInt();
-        System.out.print("Consumers count: ");
-        int consumersCount = scan.nextInt();
+            System.out.print("Producers count: ");
+            producersCount = scan.nextInt();
+            System.out.print("Consumers count: ");
+            consumersCount = scan.nextInt();
+        }
 
         final Driver driver = new Driver(producersCount, consumersCount);
         driver.run();
